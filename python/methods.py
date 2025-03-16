8aeb84aeb66675f395f4655216de209399a60e51
student1awsdas <studemtbim92@gmail.com>
2025-03-16 13:30:51 +0545

    easy

---
+++ python/methods.py
@@ -0,0 +1,28 @@
+import numpy as np
+a=np.array([1,2,3,4,5])
+
+print(a)
+
+# median
+
+print('Median:', np.median(a))
+
+# mean
+
+print('Mean:', np.mean(a))
+
+# standard deviation
+
+print('Standard Deviation:', np.std(a))
+
+# max
+
+print('Max:', np.max(a))
+
+# min
+
+print('Min:', np.min(a))
+
+# percentile
+
+print('Percentile:', np.percentile(a, q=50))
