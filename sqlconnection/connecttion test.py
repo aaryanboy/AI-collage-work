b9c1c1e3b304f250dcc5eeab23e8e6e14cd4ecc0
student1awsdas <studemtbim92@gmail.com>
2025-03-16 13:02:40 +0545

    hello workd

---
+++ sqlconnection/connecttion test.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+# Establish connection
+conn = mysql.connector.connect(
+    host="localhost",       # or your server's address
+    user="root",    # MySQL username
+    password="", # MySQL password
+    database="mydatabasepython"  # Database name
+)
+
+print("Connected successfully!")
